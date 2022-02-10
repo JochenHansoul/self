97c85df418c9ca414d3738afb2dfc146789ddc91
JochenHansoul <jochen.hansoul@outlook.be>
2022-02-10 13:45:59 +0100

    Loaded all the modules into the html file and managed to correctly get an user object with javascript.

--- javascript/user/js/get-user.js
+++ javascript/user/js/get-user.js
@@ -1,16 +1,23 @@
 "use strict";
 
 // modules
-import { getFormObject } from "./modules/get-form-object.js";
-import { sendFetch } from "./modules/send-fetch.js";
+//import { getFormObject } from "./modules/get-form-object.js";
+//import { sendFetch } from "./modules/send-fetch.js";
+
+// functions
+const printResponse = x => console.log(x);
+const printError = x => console.log(x);
 
 // constants
 const USER_FORM = document.forms.user;
 
 //main
 USER_FORM.addEventListener("submit", event=> {
+  const obj = getFormObject(USER_FORM);
+  console.log(obj);
+  sendGet(
+    USER_FORM.action + "/" + obj.id,
+    printResponse,
+    printError);
   event.preventDefault();
-  window.history.back();
-  const obj = getFormObject(USER_FORM),
-  sendFetch(USER_FORM.action + "/" + obj.id);
 });
