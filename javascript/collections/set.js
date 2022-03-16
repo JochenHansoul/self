fd2ab08d8fb173d5a59ba3b8dc61026c923db7d8
JochenHansoul <jochen.hansoul@outlook.be>
2022-03-16 15:18:37 +0100

    More test on set in JavaScript (adding multiple sets together).

--- javascript/collections/set.js
+++ javascript/collections/set.js
@@ -4,11 +4,22 @@ const set = new Set([1, 2, 3, 4]);
 const set2 = new Set(set);
 set2
   .add(5)
-  .add(6);
-set2
-  .delete(2);
+  .add(6, 7); // only ads first value
 
 console.log("original set:");
 console.log(set);
 console.log("set made from other set:");
-console.log(set2);
+console.log(set2.values());
+
+console.log("adding two sets together:");
+console.log(new Set([...set, ...set2, ...new Set([9, 10])]));
+
+// adding a list of sets together
+const sets = [
+  new Set([1, 2]),
+  new Set([3, 4]),
+  new Set([1, 7])
+];
+
+const v = sets.reduce((acc, cur) => new Set([...acc, ...cur]), new Set());
+console.log(v);
