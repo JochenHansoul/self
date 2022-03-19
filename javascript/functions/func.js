da5aa087d1c82d1ead332cca2d8a41f92ac4bd89
JochenHansoul <jochen.hansoul@outlook.be>
2022-03-19 22:28:37 +0100

    Some tests on the return value of returned functions.

---
+++ javascript/functions/func.js
@@ -0,0 +1,14 @@
+const f1 = (function() {
+  function configurationManager() {};
+  configurationManager.getConfig = function innerFunction() {};
+  return configurationManager;
+}());
+
+const f2 = (function() {
+  return (new Function("configurationManager")).getConfig = function innerFunction() {};
+  return (new Function().setName = "name").getConfig = function innerFunction() {};
+})();
+
+console.log(f1.name);
+console.log(f2.name);
+console.log(new Function(function hello() {}));
