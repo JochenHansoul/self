d70ff3cadb1021ec1fc6a069c6d703d62529e735
JochenHansoul <jochen.hansoul@outlook.be>
2022-03-21 20:15:38 +0100

    Added code to show where the error is.

--- java/JavaWordGame/src/main/java/com/codesse/codetest/wordgame/App.java
+++ java/JavaWordGame/src/main/java/com/codesse/codetest/wordgame/App.java
@@ -2,13 +2,17 @@ package com.codesse.codetest.wordgame;
 
 public class App {
     public static void main(String[] args) {
-        WordGameImpl wordGame = new WordGameImpl("worldgoodafsfoasifeafnsiorfsafeoaiaaaawweoifaoirwoooooeeeefffuuuuiiiii", new ValidWordsImpl());
+        WordGameImpl wordGame = new WordGameImpl("helloworldgoodbananapeerafsfoasifeafnsiorfsafeoaiaaaawweoifaoirwoooooeeeefffuuuuiiiii", new ValidWordsImpl());
 
-        System.out.println(wordGame.submitWord("p1", "aa"));
-        System.out.println(wordGame.submitWord("p2", "world"));
-        System.out.println(wordGame.submitWord("p3", "good"));
+        System.out.println(wordGame.submitWord("p1", "aa")); // 2
+        System.out.println(wordGame.submitWord("p2", "world")); // 5
+        System.out.println(wordGame.submitWord("p3", "good")); // 4
+        System.out.println(wordGame.submitWord("p4", "banana")); // 6
+        System.out.println(wordGame.submitWord("p5", "peer")); // 4 --> 0
+        System.out.println(wordGame.submitWord("p6", "world")); // 5 --> 0
 
-        int i = 2;
+
+        int i = 0;
         System.out.println(wordGame.getPlayerNameAtPosition(i));
         System.out.println(wordGame.getWordEntryAtPosition(i));
         System.out.println(wordGame.getScoreAtPosition(i));
