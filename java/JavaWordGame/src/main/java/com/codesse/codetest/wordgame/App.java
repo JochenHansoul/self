e3edaf4eae3b3212053570e6524012dbe62a703a
JochenHansoul <jochen.hansoul@outlook.be>
2022-03-23 23:17:40 +0100

    Tested the code in WordGameImpl.

--- java/JavaWordGame/src/main/java/com/codesse/codetest/wordgame/App.java
+++ java/JavaWordGame/src/main/java/com/codesse/codetest/wordgame/App.java
@@ -2,19 +2,19 @@ package com.codesse.codetest.wordgame;
 
 public class App {
     public static void main(String[] args) {
-        WordGameImpl wordGame = new WordGameImpl("helloworldgoodbananapeerafsfoasifeafnsiorfsafeoaiaaaawweoifaoirwoooooeeeefffuuuuiiiii", new ValidWordsImpl());
+        WordGameImpl wordGame = new WordGameImpl("boonhelloworldgoodbananapeerafsfoasifeafnsiorfsafeoaiaaaawweoifaoirwoooooeeeefffuuuuiiiii", new ValidWordsImpl());
 
         System.out.println(wordGame.submitWord("p1", "aa")); // 2
         System.out.println(wordGame.submitWord("p2", "world")); // 5
+        System.out.println(wordGame.submitWord("p5", "peer")); // 4
+        System.out.println(wordGame.submitWord("p3", "boon")); // 4
         System.out.println(wordGame.submitWord("p3", "good")); // 4
         System.out.println(wordGame.submitWord("p4", "banana")); // 6
-        System.out.println(wordGame.submitWord("p5", "peer")); // 4 --> 0
-        System.out.println(wordGame.submitWord("p6", "world")); // 5 --> 0
+        System.out.println(wordGame.submitWord("p6", "world")); // 0
 
-
-        int i = 0;
-        System.out.println(wordGame.getPlayerNameAtPosition(i));
-        System.out.println(wordGame.getWordEntryAtPosition(i));
-        System.out.println(wordGame.getScoreAtPosition(i));
+        // The words with the same length get added based on alphabetical position and not on when added.
+        for (int i = 0; i < 6; i++) {
+            System.out.println(wordGame.getWordEntryAtPosition(i));
+        }
     }
 }
