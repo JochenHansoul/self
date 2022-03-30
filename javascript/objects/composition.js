401193c6f8f8a1b61fbd7e01c2d9fc83289cbf77
JochenHansoul <jochen.hansoul@outlook.be>
2022-03-30 23:36:22 +0200

    Added extra comments, removed unnecessary comments and added a test with a composite object with that uses composite functions with the same methods.

--- javascript/objects/composition.js
+++ javascript/objects/composition.js
@@ -1,5 +1,6 @@
 "use strict";
 
+// composite functions
 // this function returns an object with a method in
 const barker = state => ({
   bark: () => "Woof, I'm " + state.name
@@ -12,6 +13,13 @@ const driver = state => ({
   }
 });
 
+const driver2 = state => ({
+  getPosition: () => state.position,
+  drive: () => {
+    state.position = state.position + state.speed * 2;
+  }
+});
+
 // composition object
 const robotDog = (name) => {
   const state = {
@@ -19,14 +27,12 @@ const robotDog = (name) => {
     speed: 100,
     position: 0
   };
-  return Object.assign({}, barker(state), driver(state));
+  return Object.assign({}, barker(state), driver(state), driver2(state));
 };
 
-//const response = barker({name: "fido"}).bark();
-//console.log(response);
-
+// main
 const robotDog1 = robotDog("fido");
 console.log(robotDog1.bark());
 console.log(robotDog1.getPosition());
-robotDog1.drive();
+robotDog1.drive(); // driver2 method is used
 console.log(robotDog1.getPosition());
