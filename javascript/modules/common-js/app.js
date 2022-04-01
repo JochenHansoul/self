dc2adb96c392d5a366b2ae581dfc793a3c07e66d
JochenHansoul <jochen.hansoul@outlook.be>
2022-04-01 23:14:58 +0200

    Some experimenting with JavaScript modules.

---
+++ javascript/modules/common-js/app.js
@@ -0,0 +1,5 @@
+const { var1, var3 } = require("./mod.js");
+//console.log(fun(1));
+console.log(var1);
+//console.log(var2);
+console.log(var3);
---
+++ javascript/modules/common-js/mod.js
@@ -0,0 +1,5 @@
+//module.exports = (number) => number + 1;
+//module.exports = "ABCDE";
+exports.var1 = "var1";
+exports.var2 = "var2";
+exports.var3 = "var3";
