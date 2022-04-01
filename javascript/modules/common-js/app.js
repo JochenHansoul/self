0e32ca2e352d7fbea19ab834925840bd98bdf044
JochenHansoul <jochen.hansoul@outlook.be>
2022-04-01 23:56:18 +0200

    Test with require.cache

--- javascript/modules/common-js/app.js
+++ javascript/modules/common-js/app.js
@@ -1,3 +1,6 @@
+console.log(require.cache);
+// if require.cache is placed after the require
+// it will also print path to the required modules
 const { var1, var3 } = require("./mod.js");
 //console.log(fun(1));
 console.log(var1);
