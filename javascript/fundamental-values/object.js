b92d9bd840bcfb479aaa5968d3d45c3f19e7e9e7
JochenHansoul <jochen.hansoul@outlook.be>
2022-04-11 22:02:36 +0200

    Added some more tests with te Object.

--- javascript/fundamental-values/object.js
+++ javascript/fundamental-values/object.js
@@ -1,6 +1,9 @@
+"use strict";
+
 console.log(Object, typeof Object);
 console.log(Object(), typeof Object());
 console.log(new Object(), typeof new Object());
+console.log(Object.create(null), typeof Object.create(null));
 
 console.log("\nprototype:");
 console.log(Object.prototype, typeof Object.prototype);
