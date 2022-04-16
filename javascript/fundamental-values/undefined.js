a9cfe0ecfe3c4cf1e3a01cd70d4ce1ddda9bd3de
JochenHansoul <jochen.hansoul@outlook.be>
2022-04-16 23:46:55 +0200

    Test with undefined values.

---
+++ javascript/fundamental-values/undefined.js
@@ -0,0 +1,6 @@
+//console.log(hi); // ReferenceError: hi is not defined
+//const hoy; // SyntaxError: Missing initializer in const declaratio
+let hoy;
+console.log(hoy);
+const no = null;
+console.log(no);
