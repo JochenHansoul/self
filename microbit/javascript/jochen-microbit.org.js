da4725266b6747cfd7a88fe342e75e788496d6fe
JochenHansoul <jochen.hansoul@outlook.be>
2022-05-30 00:36:58 +0200

    Made code js lint correct.

--- microbit/javascript/jochen-microbit.org.js
+++ microbit/javascript/jochen-microbit.org.js
@@ -1,34 +1,31 @@
+const printWord = function (word: string) {
+    basic.showString(word);
+    radio.sendString(word);
+};
 
-const print = (word: string) => {
-    basic.showString(word)
-    radio.sendString(word)
-}
+const printNumbers = function (numbers: Array<number>) {
+    numbers.forEach(n => basic.showNumber(n));
+    basic.clearScreen();
+};
 
-const printNumbers = (numbers: Array<number>) => {
-    numbers.forEach(n => basic.showNumber(n))
-    basic.clearScreen()
-}
-
-const beatHeart = (n: number = 1) => {
+const beatHeart = function (n: number = 1) {
     for (let i = 0; i < n; i++) {
-        basic.showIcon(IconNames.SmallHeart)
-        basic.showIcon(IconNames.Heart)
+        basic.showIcon(IconNames.SmallHeart);
+        basic.showIcon(IconNames.Heart);
     }
-}
+};
 
-const sayGoodbyeAndPrintNumbers = () => {
-    print("Goodbye")
-    printNumbers([0, 1, 2, 3])
-}
+const sayGoodbyeAndPrintNumbers = function () {
+    printWord("Goodbye");
+    printNumbers([0, 1, 2, 3]);
+};
 
-input.onButtonPressed(Button.A, () => {
-    print("Hello")
-    beatHeart()
-    basic.clearScreen()
-})
+input.onButtonPressed(Button.A, function () {
+    printWord("Hello");
+    beatHeart();
+    basic.clearScreen();
+});
 
-input.onButtonPressed(Button.B, sayGoodbyeAndPrintNumbers)
+input.onButtonPressed(Button.B, sayGoodbyeAndPrintNumbers);
 
-basic.forever(function () {
-	
-})
+basic.forever(() => {});
